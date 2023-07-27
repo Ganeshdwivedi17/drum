6d9e37cf21c9a0e89979dcc95f36470d262deaf3
Ganeshdwivedi17 <139634932+Ganeshdwivedi17@users.noreply.github.com>
2023-07-27 11:17:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var numberofbuttons = document.querySelectorAll(".drum").length;
 for(var i = 0; i<numberofbuttons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-  var beat = new Audio('D:\Web development My\JavaScript\Drum Kit\Drum Kit Starting Files\sounds\crash.mp3');
+  var beat = new Audio('sounds\crash.mp3');
   beat.play();
   alert("working");
 
