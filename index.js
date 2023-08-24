91fe5a43034047c3bb2937b5fea1ddb8542c835a
Ganeshdwivedi17 <139634932+Ganeshdwivedi17@users.noreply.github.com>
2023-08-24 10:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ for (var i = 0; i < numberofbuttons; i++) {
 
     var buttonInnerHTML = this.innerHTML;
     makesound(buttonInnerHTML);
-
+    animation(buttonInnerHTML);
 
   });
 }
 
 document.addEventListener("keypress",function(event) {
   makesound(event.key);
+  animation(event.key);
   });
 
 
@@ -20,40 +21,49 @@ function makesound(key){
 
   switch (key) {
     case 'w':
-      var W = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
+      var W = new Audio("sounds/tom-1.mp3");
       W.play();
       break;
 
     case 'a':
-      var A = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
+      var A = new Audio("sounds/tom-2.mp3");
       A.play();
       break;
 
     case 's':
-      var S = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
+      var S = new Audio("sounds/tom-3.mp3");
       S.play();
       break;
 
     case 'd':
-      var D = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3");
+      var D = new Audio("sounds/tom-4.mp3");
       D.play();
       break;
 
     case 'j':
-      var J = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3");
+      var J = new Audio("sounds/snare.mp3");
       J.play();
       break;
 
     case 'k':
-      var K = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3");
+      var K = new Audio("sounds/crash.mp3");
       K.play();
       break;
 
     case 'l':
-      var L = new Audio("https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3");
+      var L = new Audio("sounds/kick-bass.mp3");
       L.play();
       break;
 
       default: console.log(buttonInnerHTML);
   }
+}
+
+function animation(currentkey){
+var animationbutton = document.querySelector("." + currentkey);
+animationbutton.classList.add("pressed");
+
+setTimeout( function(){
+  animationbutton.classList.remove("pressed");
+  },100)
 }
